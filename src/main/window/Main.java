9d852c8180263de810c5c4d98cf05b19012599b0
dagiel <patryk.dagiel@gmail.com>
2018-04-10 16:08:11 +0200

    Pierwsza zmianka

--- src/main/window/Main.java
+++ src/main/window/Main.java
@@ -12,12 +12,127 @@ public class Main extends Application {
     public void start(Stage primaryStage) throws Exception{
         Parent root = FXMLLoader.load(getClass().getResource("sample.fxml"));
         primaryStage.setTitle("Hello World");
-        primaryStage.setScene(new Scene(root, 300, 275));
+        primaryStage.setScene(new Scene(root, 500, 375));
         primaryStage.show();
     }
+
+
 
 
     public static void main(String[] args) {
         launch(args);
     }
 }
+
+//public class main_form implements ActionListener {
+//
+//    private JLabel jlab;
+//    public JTextArea testOutput;
+//
+//    public main_form() {
+//
+//        JFrame jfrm = new JFrame("Egate GUI");
+//        jfrm.setSize(400,400);
+//        jfrm.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+//
+//        jlab = new JLabel();
+//        testOutput = new JTextArea(10,10);
+//
+//        JMenuBar jmb = new JMenuBar();
+//
+//        // Zakladka Plik w pasku menu
+//        JMenu jmFile = new JMenu("File");
+//        JMenuItem item_1 = new JMenuItem("Import LUA File");
+//        JMenuItem item_2 = new JMenuItem("Close EGate GUI");
+//        jmFile.add(item_1);
+//        jmFile.add(item_2);
+//        jmb.add(jmFile);
+//
+//        // Zakladka opcji w pasku menu
+//        JMenu jmOptions = new JMenu("Options");
+//        JMenuItem item_o_revive = new JMenuItem("Setup EGate connections");
+//        JMenuItem item_o_2 = new JMenuItem("Close EGate connections");
+//        JMenuItem item_o_3 = new JMenuItem("Start KPI gathering");
+//        jmOptions.add(item_o_revive);
+//        jmOptions.add(item_o_2);
+//        jmOptions.add(item_o_3);
+//        jmb.add(jmOptions);
+//
+//        jfrm.add(jlab);
+//        jfrm.add(testOutput);
+//
+//        item_1.addActionListener(this);
+//        item_2.addActionListener(this);
+//        item_o_revive.addActionListener(new ActionListener() {
+//            @Override
+//            public void actionPerformed(ActionEvent e) {
+//                String[] commands = {"/bin/bash", "-c", "sudo ./edaemon --port 10000 && sudo ./edaemon --port 30000 && sleep 1"};
+//                String[] command_2 = {"/bin/bash", "-c", "sudo ./edaemon --port 30000"};
+//                String[] command_3 = {"/bin/bash", "-c", "sudo sleep 1"};
+//                Process p, p2;
+////                try {
+////                    // Proba wykonania komendy revive_all [postawienie daemonow egate, portów itd]
+//////                    p = Runtime.getRuntime().exec("sudo ./edaemon --port 10000 && sudo ./edaemon --port 30000 && ./egate --port 20000 && sleep 1 && telnet 0 20000");
+////
+////                    p = Runtime.getRuntime().exec(commands);
+////                    p2 = Runtime.getRuntime().exec(command_2);
+////                    p = Runtime.getRuntime().exec(command_3);
+////                    BufferedReader stdInput = new BufferedReader(new InputStreamReader(p2.getInputStream()));
+////                    p.waitFor();
+////                    p2.waitFor();
+////                    String line = null;
+////                    System.out.println("Executing command : ");
+////                    while((line = stdInput.readLine()) != null) {
+////                        System.out.println(line);
+////                    }
+////                    p.destroy();
+////                    p2.destroy();
+////
+//////                    TelnetClient polaczenie_testowe = new TelnetClient();
+//////                    polaczenie_testowe.execute(testOutput);
+////
+////
+////                } catch (Exception x) {
+////                    System.out.println("To jest badziewie " + x);
+////                }
+//
+//                try {
+//                    TelnetClient polaczenie_testowe = new TelnetClient();
+//                    polaczenie_testowe.execute(testOutput);
+//                } catch (Exception x) {
+//                    System.out.println("Blad bloku try dla Socketa " + x);
+//                }
+//
+//
+//            }
+//        });
+//        item_o_2.addActionListener(this);
+//        item_o_3.addActionListener(this);
+//
+//        jfrm.setJMenuBar(jmb);jfrm.setVisible(true);
+//
+//    }
+//
+//
+//    public static void main(String[] args) {
+//        SwingUtilities.invokeLater(new Runnable() {
+//            @Override
+//            public void run() {
+//                new main_form();
+//            }
+//        });
+//    }
+//
+//    @Override
+//    public void actionPerformed(ActionEvent e) {
+//        String option = e.getActionCommand();
+//        if(option.equals("Close EGate GUI")){
+//            System.exit(0);
+//        }
+//        jlab.setText("The option chosen is " + option);
+//
+//    }
+//
+//
+//
+//}
